5d59adc258aa50c3476d1158923bd61e13caf7ad
Denis <den.pashkov.98@gmail.com>
2022-04-17 22:40:30 +0300

    spa

--- spa_python/spa_app/views.py
+++ spa_python/spa_app/views.py
@@ -21,14 +21,6 @@ class Table_view(ListView):
     def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any):
         response = super().dispatch(request, *args, **kwargs)
 
-        def clean_filter_text_to_sort():
-            if (user_form.data['find_text'] == ''):
-                return True
-
-        def clean_all():
-            if user_form.data['find_text'] != '' and user_form.data['filter_condition'] != '-':
-                return True
-
         def isfloat(a):
             try:
                 z = float(a)
@@ -36,19 +28,15 @@ class Table_view(ListView):
             except:
                 return False
 
-        def eq():
-            if user_form.data['filter_condition'] == '-' and user_form.data['find_text'] != '':
-                return True
-
         if request.is_ajax():
             user_form = table_form(request.GET)
             quer = ''
             if user_form.data['column'] != '-':
-                if clean_filter_text_to_sort():
+                if user_form.data['find_text'] == '':
                     quer = Table.objects.order_by(
                         f"{user_form.data['column']}")[:int(user_form.data['paginate_field'])]
 
-                if eq() and quer == '':
+                if user_form.data['filter_condition'] == '-' and user_form.data['find_text'] != '' and quer == '':
                     if user_form.data['column'] != 'name' and not isfloat(user_form.data['find_text']):
                         err = {
                             'ValueError': 'The value must be a number or a float'}
@@ -58,7 +46,7 @@ class Table_view(ListView):
                         quer = Table.objects.raw(
                             f"SELECT * FROM public.spa_app_table WHERE {user_form.data['column']} = '{user_form.data['find_text']}' order by {user_form.data['column']} limit {user_form.data['paginate_field']};")
 
-                if clean_all() and quer == '':
+                if user_form.data['find_text'] != '' and user_form.data['filter_condition'] != '-' and quer == '':
                     if user_form.data['filter_condition'] != 'include' and user_form.data['column'] != 'name':
                         if not isfloat(user_form.data['find_text']):
                             err = {
